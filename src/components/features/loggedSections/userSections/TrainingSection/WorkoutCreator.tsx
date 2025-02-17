35304b0a4436b285b64a840e82026408cc57d063
adrianstopa15 <adrians@student.kul.pl>
2025-02-17 19:31:10 +0100

    feat(trainingCreator): add UI tabs for training components

--- src/components/features/loggedSections/userSections/TrainingSection/WorkoutCreator.tsx
+++ src/components/features/loggedSections/userSections/TrainingSection/WorkoutCreator.tsx
@@ -14,19 +14,57 @@ export default function WorkoutCreator() {
   }
   return (
     <div className="bgLogged">
-      <div className={`containerCreator "pt-32" pt-8`}>
-        <div className="workoutCreatorElement">
-          <h1 className="lg:text-3xl mb-10 mt-6">Brak pomysłu na trening?</h1>
-          <img src={CreateWorkoutIcon} className="h-24 mb-8" />
-          <h2 className="lg:text-2xl mb-4">
-            Ułożymy go specjalnie do Twoich potrzeb!
+      <div className="containerCreator">
+        <div className="workoutCreatorElement bgLinearGreen">
+          <h1 className="lg:text-2xl mb-10 mt-6">Moje zestawy treningowe</h1>
+          <img src={CreateWorkoutIcon} className="workoutIcon mb-8" />
+          <h2 className="text-md max-h-6 p-1">
+            Przeglądaj swoje zestawy treningowe
           </h2>
-          <p className="mx-4">
+
+          <NavLink className="navlink mt-8" to="../autoTrainingCreator">
+            <button className="button-green-dark lg:text-lg  mb-8">
+              Rozpocznij
+            </button>
+          </NavLink>
+        </div>
+        <div className="workoutCreatorElement bgLinearBlue">
+          <h1 className="lg:text-2xl mb-10 mt-6">Stwórz zestaw treningowy</h1>
+          <img src={CreateWorkoutIcon} className="workoutIcon  mb-8" />
+          <h2 className="text-md max-h-6 p-1">
+            Ułóż swój własny zestaw treningowy który może posłużyć Ci w
+            następnym treningu
+          </h2>
+
+          <NavLink className="navlink mt-8" to="../autoTrainingCreator">
+            <button className="button-aqua-dark lg:text-lg   mb-8">
+              Rozpocznij
+            </button>
+          </NavLink>
+        </div>
+        <div className="workoutCreatorElement bgLinearOrange">
+          <h1 className="lg:text-2xl mb-10 mt-6">Brak pomysłu na trening?</h1>
+          <img src={CreateWorkoutIcon} className="workoutIcon mb-8" />
+          <h2 className="text-md max-h-6 p-1">
             Dzięki naszym inteligentnym algorytmom, ułożymy trening biorąc pod
             uwagę Twoje predyspozycje oraz preferencję
-          </p>
-          <NavLink to="../autoTrainingCreator">
-            <button className="button-blue lg:text-lg mt-8 mb-8">
+          </h2>
+
+          <NavLink className="navlink mt-8" to="../autoTrainingCreator">
+            <button className="button-orange-dark lg:text-lg  mb-8">
+              Rozpocznij
+            </button>
+          </NavLink>
+        </div>
+        <div className="workoutCreatorElement bgLinearPurple">
+          <h1 className="lg:text-2xl mb-10 mt-6">Gotowe plany treningowe</h1>
+          <img src={CreateWorkoutIcon} className="workoutIcon mb-8" />
+          <h2 className="text-md max-h-6 p-1">
+            Przeglądaj gotowe plany treningowe i dodaj je do swojej kolekcji.
+          </h2>
+
+          <NavLink className="navlink mt-8" to="../autoTrainingCreator">
+            <button className="button-purple-dark lg:text-lg  mb-8">
               Rozpocznij
             </button>
           </NavLink>
